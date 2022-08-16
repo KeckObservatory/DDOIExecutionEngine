7c59743b457d8bdd6de32aa88a957aa9516f62a9
tylertucker202 <tylertucker202@gmail.com>
2022-08-16 13:01:54 -1000

    adding queues to xcute

--- client-xcute.py
+++ client-xcute.py
@@ -1,6 +1,16 @@
 import socketio
+from ExecutionEngine import *
 
-def define_routes(sio):
+def main():
+    
+    host = '0.0.0.0'
+    port = '50007'
+    url = 'http://' + host + ':' + port
+
+    observing_queue = BaseQueue(ObservingBlockItem)
+    sequence_queue = BaseQueue(SequenceItem)
+    event_queue = BaseQueue(EventItem)
+
     @sio.event
     def connect():
         print('connection established')
@@ -8,14 +18,20 @@ def define_routes(sio):
     @sio.event
     def sequence_queue_to_xcute(data):
         print('sequence_queue_to_xcute recieved', data)
+        newSequenceQueue = data.get('sequence_queue')
+        sequence_queue.set_queue(newSequenceQueue)
 
     @sio.event
     def event_queue_to_xcute(data):
         print('event_queue_to_xcute recieved', data)
+        newEventQueue = data.get('event_queue')
+        event_queue.set_queue(newEventQueue)
 
     @sio.event
     def ob_to_xcute(data):
         print('ob_to_excute recieved', data)
+        newOB = data.get("ob")
+        observing_queue.seq_queue(newOB)
 
     @sio.event
     def task_to_xcute(data):
@@ -25,13 +41,8 @@ def define_routes(sio):
     def disconnect():
         print('disconnected from server')
 
-def main():
-    
-    host = '0.0.0.0'
-    port = '50007'
-    url = 'http://' + host + ':' + port
-    sio = socketio.Client(logger=True, engineio_logger=True)
-    define_routes(sio)
+    #sio = socketio.Client(logger=True, engineio_logger=True)
+    sio = socketio.Client()
     sio.connect(url)
     sio.wait()
 
