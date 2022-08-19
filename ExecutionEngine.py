2737b8a381c719365b7d50bd9002b6685790f605
Max Brodheim <max.brodheim@gmail.com>
2022-08-19 13:24:50 -1000

    flesh out structure and event execution

--- ExecutionEngine.py
+++ ExecutionEngine.py
@@ -1,4 +1,25 @@
-from multiprocessing import Process
+"""
+Execution Engine Base Script. Key components are:
+
+    - The three queues:
+        - Observing Queue (contains OBs)
+        - Sequence Queue (contains sequences)
+        - Event Queue (contains individual events)
+    - To go along with each queue, a "transfer function" that takes something
+      from a given queue, and transfers it to something that the next queue/step
+      can understand:
+        - OB_to_sequences() takes an OB, and outputs a list of sequences
+        - sequence_to_events() takes a single sequence, and outputs a list of
+          events
+        - dispatch_event() takes an event, and sends it to an EventExecutor for
+          execution
+"""
+
+from multiprocessing import Process, Pipe
+from multiprocessing.connection import Connection
+from typing import Tuple
+
+from EventExecutor import EventExecutor
 from Queues.BaseQueue import DDOIBaseQueue
 from Queues.ObservingBlockItem import ObservingBlockItem
 from Queues.SequenceItem import SequenceItem
@@ -13,7 +34,7 @@ def OB_to_sequence() -> None:
     OB = observing_queue.get()
     sequence_queue.put_many(OB_to_sequence(OB))
 
-def OB_to_sequence(OB) -> list:
+def OB_to_sequences(OB) -> list:
     """Takes an OB item from a queue and converts it to a list of sequences
 
     Parameters
@@ -31,6 +52,8 @@ def OB_to_sequence(OB) -> list:
     for seq in OB.sequences:
         output.append(SequenceItem.from_sequence(seq))
     return output
+
+
 
 def sequence_to_events(sequence, script) -> list:
     """Takes a sequence and breaks it down into executable events for the 
@@ -57,7 +80,7 @@ def sequence_to_events(sequence, script) -> list:
     """
     return []
 
-def event_dispatcher(event_item) -> Process:
+def event_dispatcher(event_item) -> Tuple[Process, Connection]:
     """Takes an event item and dispatches it to a process
 
     Parameters
@@ -67,8 +90,21 @@ def event_dispatcher(event_item) -> Process:
 
     Returns
     -------
-    Process
-        multiprocessing Process that is running the event
+    Tuple[Process, Connection]
+        multiprocessing Process that is running the event, Pipe connection
     """
+    def process_task(args) -> None:
+        # Create an Event Executor object
+        executor = EventExecutor(args["connection"], args["event_args"])
+        executor.run()
 
-    
+    # Create a Pipe object for communication
+    parent_connection, child_connection = Pipe()
+
+    process_args = {
+        "connection":child_connection
+    }
+
+    p = Process(target=process_task, args = process_args)
+
+    return p, parent_connection
