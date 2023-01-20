cf6ba097361a0ea317c38cd12490957b03aef9d9
Max Brodheim <max.brodheim@gmail.com>
2023-01-20 13:56:44 -1000

    event dispatching

--- Queues/EventQueue/EventItem.py
+++ Queues/EventQueue/EventItem.py
@@ -2,5 +2,8 @@ from ..QueueItem import QueueItem
 
 class EventItem(QueueItem):
     
-    def __init__(self) -> None:
+    def __init__(self, args, func, ddoi_config) -> None:
         super().__init__()
+        self.args = args
+        self.func = func
+        self.ddoi_config = ddoi_config
--- Queues/EventQueue/EventQueue.py
+++ Queues/EventQueue/EventQueue.py
@@ -1,10 +1,21 @@
+import importlib
+
 from Queues.BaseQueue import DDOIBaseQueue
 from Queues.EventQueue.EventItem import EventItem
+
 class ObservingQueue(DDOIBaseQueue):
 
-    def __init__(self, name=None) -> None:
+    def __init__(self, logger, name=None) -> None:
         item_type = EventItem
         super().__init__(item_type, name)
+
+        self.flags = {
+            "block" : True,
+            "wait_for_config" : True
+        }
+
+        self.logger = logger
+        self.ddoi_config = {}
 
     def get_script(self, sequence):
         """Fetch a DDOI script for this sequence
@@ -14,7 +25,7 @@ class ObservingQueue(DDOIBaseQueue):
         sequence : SequenceItem
             Sequence that this function should find a script for
         """
-        pass
+        return[]
 
     def load_events_from_sequence(self, sequence):
         """Takes a sequence and breaks it down into executable events this queue
@@ -40,4 +51,38 @@ class ObservingQueue(DDOIBaseQueue):
         list
             Python list of events
         """
+        
+        instrument = sequence['metadata']['instrument']
+
+        # How do I get the script?
+        script = []
+
+        for el in script:
+            if not self.ddoi_config['keywords'].has_key(el):
+                self.logger.error(f"Invalid script option encountered: {el}")
+                raise Exception() # TODO: figure out what to do with this
+            
+            try:
+                tm_name = f"{self.ddoi_cfg['translators'][instrument]}.ddoi_script_functions"
+                module = importlib.import_module(tm_name)
+
+                try:
+                    f = getattr(module, el)
+                except AttributeError as e:
+                    self.logger.error(f"Failed to find {el} within the {tm_name} module")
+                    raise Exception() # TODO: figure out what to do with this
+                
+                event = EventItem(args=sequence.parameters, func=f)
+                self.put_one(event)
+
+            except ImportError as e:
+                self.logger.error(f"Error while importing!")
         self.put_many([])
+
+    def dispatch_event(self):
+        """Pull the top element of this queue and put it into the executing
+        area, after checking for the appropriate flags
+        """
+        event = self.get()
+
+        
