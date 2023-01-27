8931421b43602aa7817f7e70b083d9c1c149c10b
Tyler Tucker <tylertucker202@gmail.com>
2023-01-27 10:53:09 -1000

    restructure inputs. using logging module

--- interface/client-xcute.py
+++ interface/client-xcute.py
@@ -1,40 +1,41 @@
 import socketio
 from core.ExecutionEngine import ExecutionEngine 
 import pdb
-from Queues.BaseQueue import DDOIBaseQueue
-from Queues.ObservingBlockItem import ObservingBlockItem
-from Queues.SequenceItem import SequenceItem
-from Queues.EventItem import EventItem
+import logging
+from core.Queues.BaseQueue import DDOIBaseQueue
+from core.Queues.ObservingQueue.ObservingBlockItem import ObservingBlockItem
+from core.Queues.SequenceQueue.SequenceItem import SequenceItem
+from core.Queues.EventQueue.EventItem import EventItem
 
 def main():
 
     ee = ExecutionEngine()
-    
     host = '0.0.0.0'
     port = '50007'
     url = 'http://' + host + ':' + port
 
-    #sio = socketio.Client(logger=True, engineio_logger=True)
     sio = socketio.Client()
+
     @sio.event
     def connect():
-        print('connection established')
+        logging.info('connection established')
 
     @sio.event
     def sequence_queue_to_xcute(data):
-        print('sequence_queue_to_xcute recieved', data)
+        logging.info('sequence_queue_to_xcute recieved', data)
         seqDict = data.get('sequence_queue')
         newSequenceQueue = [ SequenceItem.from_sequence(x) for x in seqDict ]
         ee.seq_q.set_queue(newSequenceQueue)
 
     @sio.event
     def event_queue_to_xcute(data):
-        print('event_queue_to_xcute recieved', data)
+        logging.info('event_queue_to_xcute recieved', data)
         eventDict = data.get('event_queue')
+        #TODO: straighten out event item (does not use from_sequence)
         newEventQueue = [ EventItem.from_sequence(x) for x in eventDict ]
         ee.ev_q.set_queue(newEventQueue)
-        print('\nevent_queue.get_queue_as_json()\n')
-        print(ee.ev_q.get_queue_as_json())
+        logging.info('event_queue.get_queue_as_json()')
+        logging.info(ee.ev_q.get_queue_as_json())
 
     @sio.event
     def ob_to_xcute(data):
@@ -45,11 +46,11 @@ def main():
 
     @sio.event
     def task_to_xcute(data):
-        print('task_to_excute recieved', data)
+        logging.info('task_to_excute recieved', data)
 
     @sio.event
     def disconnect():
-        print('disconnected from server')
+        logging.info('disconnected from server')
 
     sio.connect(url)
     sio.wait()
