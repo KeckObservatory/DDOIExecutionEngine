c645c4349beebcfa57a65d22e06e94b5a68585a5
Max Brodheim <max.brodheim@gmail.com>
2023-01-31 09:49:54 -1000

    implement "only one ob at a time"

--- core/Queues/ObservingQueue/ObservingBlockItem.py
+++ core/Queues/ObservingQueue/ObservingBlockItem.py
@@ -4,32 +4,33 @@ from ..QueueItem import QueueItem
 
 class ObservingBlockItem(QueueItem):
 
-    def __init__(self) -> None:
+    def __init__(self, OB) -> None:
         super().__init__()
-        self.metadata = None
-        self.acquisition = None
-        self.sequences = None
-        self.target = None
-        self.COMPONENTS = None
+        self.OB = OB
+    #     self.metadata = None
+    #     self.acquisition = None
+    #     self.sequences = None
+    #     self.target = None
+    #     self.COMPONENTS = None
 
-    @staticmethod
-    def create_observing_block_item(ob):
-        ret = ObservingBlockItem()
-        ret.metadata = ob.get('metadata', None)
-        ret.acquisition = ob.get('acquisition', None)
-        ret.sequences = ob.get('observations', None)
-        ret.target = ob.get('target', None)
-        ret.COMPONENTS = ['metadata', 'sequences', 'science', 'target']
-        return ret
+    # @staticmethod
+    # def create_observing_block_item(ob):
+    #     ret = ObservingBlockItem()
+    #     ret.metadata = ob.get('metadata', None)
+    #     ret.acquisition = ob.get('acquisition', None)
+    #     ret.sequences = ob.get('observations', None)
+    #     ret.target = ob.get('target', None)
+    #     ret.COMPONENTS = ['metadata', 'sequences', 'science', 'target']
+    #     return ret
 
-    @classmethod
-    def from_DICT(cls, ob):
-        ret = cls.create_observing_block_item(ob)
-        return ret
+    # @classmethod
+    # def from_DICT(cls, ob):
+    #     ret = cls.create_observing_block_item(ob)
+    #     return ret
        
 
-    @classmethod
-    def from_JSON(cls, filename):
-        ob = json.load(filename)
-        ret = cls.create_observing_block_item(ob)
-        return ret
+    # @classmethod
+    # def from_JSON(cls, filename):
+    #     ob = json.load(filename)
+    #     ret = cls.create_observing_block_item(ob)
+    #     return ret
--- core/Queues/SequenceQueue/SequenceItem.py
+++ core/Queues/SequenceQueue/SequenceItem.py
@@ -2,14 +2,15 @@ from ..QueueItem import QueueItem
 
 class SequenceItem(QueueItem):
     
-    def __init__(self) -> None:
+    def __init__(self, num) -> None:
         super().__init__()
-        self.metadata = None
-        self.parameters = None
+        self.sequence_number = num
+        # self.metadata = None
+        # self.parameters = None
 
-    @classmethod
-    def from_sequence(cls, sequence):
-        ret = SequenceItem()
-        ret.metadata = sequence['metadata']
-        ret.parameters = sequence['parameters']
-        return ret
+    # @classmethod
+    # def from_sequence(cls, sequence):
+    #     ret = SequenceItem()
+    #     ret.metadata = sequence['metadata']
+    #     ret.parameters = sequence['parameters']
+    #     return ret
