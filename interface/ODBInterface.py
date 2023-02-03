a11f74ab7f6b8c004d4782259ee38f5ffa5340c3
Max Brodheim <max.brodheim@gmail.com>
2023-02-03 11:26:53 -1000

    add detailed logging

--- interface/ODBInterface.py
+++ interface/ODBInterface.py
@@ -1,4 +1,5 @@
 from requests import Session, packages
+from requests.exceptions import JSONDecodeError
 import configparser
 
 class ODBInterface:
@@ -8,12 +9,13 @@ class ODBInterface:
         packages.urllib3.disable_warnings()
 
         self.logger = logger
+
         self.cfg = configparser.ConfigParser()
-        # logger.debug(f"Parsing config file {cfg_file}")
+        self.logger.debug(f"Parsing config file: {cfg_file}")
         self.cfg.read(cfg_file)
 
         self.api_url = self.cfg['URLS']["api_url"]
-        # logger.debug(f"Set OBDInterface base URL to {self.api_url}")
+        self.logger.debug(f"Set OBDInterface base URL to {self.api_url}")
         self.session = self.get_authenticated_session()
         
 
@@ -22,47 +24,54 @@ class ODBInterface:
         session = Session()
         session.verify = False
 
-        # url_login = self.cfg["URLS"]["login_url"]
-        # credentials = {
-        #     "email" : self.cfg["CREDENTIALS"]["email"],
-        #     "password" : self.cfg["CREDENTIALS"]["password"]
-        # }
-
-        # login_result = session.post(url=url_login, params=credentials, verify=False)
-        # if login_result.status_code == 401:
-        #     raise ConnectionRefusedError(f"Login unsuccesful: {login_result.json()['comment']}")
-        # elif login_result.status_code != 200:
-        #     raise ConnectionError(f"Unknown connection error while trying to log in")
-        
-        # cookies = login_result.json()
         cookies = {
             "ODB-API-KEY" : self.cfg["CREDENTIALS"]["APIKEY"],
             "ODB-API-UID" : self.cfg["CREDENTIALS"]["APIUID"]
         }
-        # print(cookies)
+
+        self.loggger.debug(f"Loading cookies from cfg file")
         session.cookies.update(cookies)
 
         return session
 
+
     def get_OB_from_id(self, id):
         url = f"{self.api_url}/obsBlocks"
+        self.logger.debug(f"Getting OB from {url}")
         res = self.session.get(url, params={"ob_id" : id})
         if res.status_code == 200:
+            self.logger.info(f"Pulled OB: {id} from DB")
             data = res.json()
             return data
         else:
-            print("What should I do here?")
+            self.logger.error(f"Failed to get OB from the ODB! Got status code {res.status_code}")
+            try:
+                self.logger.error(f"Message: {res.json()}")
+            except JSONDecodeError as e:
+                self.logger.error(f"No decodable JSON to log")
+        
+            raise RuntimeError("Failed to recieve an OB from the database!")
     
     def get_script(self, instrument, script_name, script_version):
         url = f"{self.api_url}/instrumentPackages/{instrument.upper()}/scripts"
+        self.logger.debug(f"Getting script from {url}")
         res = self.session.get(url)
-        print(res.json())
+
         if res.status_code == 200:
             for script in res.json():
                 if script["metadata"]["name"] == script_name and \
                     script["metadata"]["version"] == script_version:
-
                     return script["script"]
+                
+            raise NotImplementedError(f"No script matching {script_name} version {script_version} found in the ODB")
+        else:
+            self.logger.error(f"Failed to get scripts from the ODB! Got status code {res.status_code}")
+            try:
+                self.logger.error(f"Message: {res.json()}")
+            except JSONDecodeError as e:
+                self.logger.error(f"No decodable JSON to log")
+        
+            raise RuntimeError("Failed to recieve an OB from the database!")
 
     def close_session(self):
         self.session.close()
