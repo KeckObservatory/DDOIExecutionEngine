b661493f39091bb522581e3a4db644c951287e46
Tyler Tucker <tylertucker202@gmail.com>
2023-02-13 15:05:53 -1000

    adding config to setup

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setup(
               'execution_engine.core', 
               'execution_engine.interface', 
               'execution_engine.core', 
+              'execution_engine.configs', 
               'execution_engine.core.Queues',
               'execution_engine.core.Queues.SequenceQueue',
               'execution_engine.core.Queues.ObservingQueue',
               'execution_engine.core.Queues.EventQueue'],
+    package_data={'': ['*.json']},
     # package_dir={"": "execution_engine"},
     version='1.0.0'
 )
