f5d4c4731dc6edf912ecef1c7e0bc4ac06440a00
Tyler Tucker <tylertucker202@gmail.com>
2023-02-13 14:43:06 -1000

    manually adding submodules because find_packages is not working correctly

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = "execution_engine",
-    packages=find_packages(where='execution_engine', include=["interface", "core"]),
-    package_dir={"": "execution_engine"},
+    # packages=find_packages(where='execution_engine'),
+    packages=['execution_engine', 
+              'execution_engine.core', 
+              'execution_engine.interface', 
+              'execution_engine.core', 
+              'execution_engine.core.Queues.SequenceQueue',
+              'execution_engine.core.Queues.ObservingQueue',
+              'execution_engine.core.Queues.EventQueue'],
+    # package_dir={"": "execution_engine"},
     version='1.0.0'
 )
