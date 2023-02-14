edbd193b8874dce259466f885210221dac3c0737
Tyler Tucker <tylertucker202@gmail.com>
2023-02-13 14:14:54 -1000

    needed to specify base dir and include subdirs

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "execution_engine",
-    packages=find_packages(include=['execution_engine']),
+    packages=find_packages(where='execution_engine', include=["interface", "core"]),
+    package_dir={"": "execution_engine"},
     version='1.0.0'
 )
