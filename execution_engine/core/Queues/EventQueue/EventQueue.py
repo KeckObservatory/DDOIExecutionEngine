82ebe2d55b87630c6c0531c7ef6929c9405ea673
Tyler Tucker <tylertucker202@gmail.com>
2023-02-23 09:39:19 -1000

    check to see if lock is blocked

--- execution_engine/core/Queues/EventQueue/EventQueue.py
+++ execution_engine/core/Queues/EventQueue/EventQueue.py
@@ -118,7 +118,8 @@ class EventQueue(DDOIBaseQueue):
         area, after checking for the appropriate flags
         """
 
-        if self.lock.locked() and not force:
+        isBlocked = self.lock.acquire(block=False)
+        if isBlocked and not force:
             self.logger.error("Tried to fetch an event while blocked!")
             raise SystemError("Event Queue is blocked, but an event dispatch was requested")
         
