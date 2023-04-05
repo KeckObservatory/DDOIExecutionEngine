fd4ab8bcd15cb4c5b2c9faf5e5340a56970e83f5
Max Brodheim <max.brodheim@gmail.com>
2023-04-05 09:20:25 -1000

    comments and a few bugs

--- execution_engine/core/default_events/SocketEvent.py
+++ execution_engine/core/default_events/SocketEvent.py
@@ -5,6 +5,19 @@ import os
 
 
 class SocketEvent(TranslatorModuleFunction):
+    """Waiting event. This event will wait until it recieves a kill message over
+    a provided connection.
+
+    Args
+    ----
+    connection : multiprocessing.Connection
+        Multiprocessing connection object to read from
+    timeout : int
+        How long this event should wait before automatically returning. If negative,
+        infinite timeout
+    kill_message : str
+        String that should kill this event
+    """
 
     @classmethod
     def pre_condition (cls, args, logger, cfg):
@@ -12,25 +25,28 @@ class SocketEvent(TranslatorModuleFunction):
 
     @classmethod
     def perform (cls, args, logger, cfg):
-        connection = args.get('port', None)
+        connection = args.get('connection', None)
         timeout = args.get('timeout')
         if connection:
             start_time = datetime.datetime.utcnow()
             # Listen on that port until we get the message we expect, with a timeout
             while True:
                 time.sleep(.5)
-                if datetime.datetime.utcnow() - start_time > timeout:
+                if timeout > 0 and datetime.datetime.utcnow() - start_time > timeout:
                     raise TimeoutError(f"Exceeded {timeout} s timeout set for event")
                 
                 if connection.poll():
                     message = connection.read()
                     logger.debug(f"Read message {message} at {os.getpid()}")
 
-                    if message and message == args.get('message'):
+                    if message and message == args.get('kill_message'):
                         logger.info(f"Message recieved matches {os.getpid()}'s kill code. Breaking...")
                         break
         else:
-            raise ConnectionError("Failed to connect to event server")
+            if connection is None:
+                raise KeyError("No connection object in args.")
+            else:
+                raise ConnectionError("Failed to connect to event server")
                
 
     @classmethod
