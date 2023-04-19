ec86af7ccfe4969524eb50364db6336f093bd0ee
Tyler Tucker <tylertucker202@gmail.com>
2023-04-19 11:42:45 -1000

    runing in parallel

--- execution_engine/core/Queues/EventQueue/EventQueue.py
+++ execution_engine/core/Queues/EventQueue/EventQueue.py
@@ -11,7 +11,7 @@ enable_dispatching = True
 
 # This variable is used to bypass the multiprocessing queue and run 
 # events sequentially
-run_events_sequentially = True 
+run_events_sequentially = False 
 
 class EventQueue(DDOIBaseQueue):
 
